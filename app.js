57677e7b455e4f78ccd583b086fe47544e6bf18c
Your Name <you@example.com>
2022-12-14 02:12:07 +0530

    port changes

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ const commentRoute = require('./routes/comment')
 const allPostsRoute = require('./routes/all_posts')
 const followRoute = require('./routes/follow')
 const unfollowRoute = require('./routes/unfollow')
-const {authCheck} = require('./middleware/auth.js')
+const { authCheck } = require('./middleware/auth.js')
 
+const PORT = process.env.PORT || 3000;
 //Connections
 
-mongoose.set('strictQuery',false);
-mongoose.connect(process.env.MONGO_URL,()=>{
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, () => {
     console.log("connected to mongoDB")
 });
 
@@ -30,18 +31,18 @@ app.use(morgan('common'));
 
 //Routes
 
-app.use('/api/user',authCheck,userRoute);
-app.use('/api/authenticate',authRoute);
-app.use('/api/posts',authCheck,postRoute);
-app.use('/api/like',authCheck,likeRoute);
-app.use('/api/unlike',authCheck,unlikeRoute);
-app.use('/api/comment',authCheck,commentRoute);
-app.use('/api/all_posts',authCheck,allPostsRoute);
-app.use('/api/follow',authCheck,followRoute);
-app.use('/api/unfollow',authCheck,unfollowRoute);
+app.use('/api/user', authCheck, userRoute);
+app.use('/api/authenticate', authRoute);
+app.use('/api/posts', authCheck, postRoute);
+app.use('/api/like', authCheck, likeRoute);
+app.use('/api/unlike', authCheck, unlikeRoute);
+app.use('/api/comment', authCheck, commentRoute);
+app.use('/api/all_posts', authCheck, allPostsRoute);
+app.use('/api/follow', authCheck, followRoute);
+app.use('/api/unfollow', authCheck, unfollowRoute);
 
-app.listen(3000,()=>{
-    console.log("Backend server runs!");
-})
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
 
 module.exports = app;
